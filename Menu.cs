1f5144b1af27cb0304fcec0b22a864e003094715
kimidomaru <luikson97@hotmail.com>
2016-12-06 18:18:11 -0200

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+	GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		camera=GameObject.FindGameObjectWithTag("MainCamera");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InvokeRepeating("DesceMenu",4f,0.5f);
+		if(Input.GetKeyDown(KeyCode.Return)){
+			SceneManager.LoadScene("wb");
+		}
+			
+	}
+	void DesceMenu(){
+		if(camera.transform.position.y>-6.2f)
+			camera.transform.position+=new Vector3(0,-1*Time.deltaTime,0);
+	}
+}
---
+++ Plataforma2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plataforma2 : MonoBehaviour {
+	GameObject plataforma;
+	Rigidbody2D rb2d;
+	float i=0;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		plataforma = GameObject.FindGameObjectWithTag ("Plataforma2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(i<0.5f)
+			rb2d.isKinematic=true;
+		else if(i>=0.5f && i<1.5f){
+			float speed = 2*Time.deltaTime;
+			Vector3 temp = new Vector3(179f,plataforma.transform.position.y,
+			                           plataforma.transform.position.z);
+			
+			plataforma.transform.position = Vector3.MoveTowards(plataforma.transform.position,temp,speed);
+		}
+		else if(i>=1.5f && i<2.5f){
+			float speed = 2*Time.deltaTime;
+			Vector3 temp2 = new Vector3(176f,plataforma.transform.position.y,
+			                            plataforma.transform.position.z);
+			plataforma.transform.position = Vector3.MoveTowards(plataforma.transform.position,temp2,speed);
+		}
+		else if(i>=2.5f){
+			rb2d.isKinematic=false;
+		}
+	}
+	void OnCollisionEnter2D(Collision2D outro){
+		if(outro.gameObject.tag.Equals("Player")){
+			i=Random.Range(0f,3f);
+		}
+	}
+	void OnTriggerEnter2D(Collider2D outro){
+		if(outro.gameObject.tag.Equals("reset")){
+			Destroy(gameObject);
+		}
+	}
+}
